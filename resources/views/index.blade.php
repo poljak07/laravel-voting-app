70429dfd0251e4b3aa82076f42d50a3c63edfb26
poljak07 <acmeta4@gmail.com>
2024-11-27 17:05:37 +0100

    Make Responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-    <div class="filters flex space-x-6 ">
-        <div class="w-1/3 ">
+    <div class="filters flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-6 ">
+        <div class="w-full md:w-1/3 ">
             <select name="category" id="category" class="w-full rounded-xl px-4 py-2 border-none">
                 <option value="Category One">Category One</option>
                 <option value="Category One">Category Two</option>
@@ -10,7 +10,7 @@
             </select>
         </div>
 
-        <div class="w-1/3 ">
+        <div class="w-full md:w-1/3 ">
             <select name="other_filters" id="other_filters" class="w-full rounded-xl px-4 py-2 border-none">
                 <option value="Category One">Filter One</option>
                 <option value="Category One">Filter Two</option>
@@ -19,7 +19,7 @@
 
             </select>
         </div>
-    <div class="w-2/3 relative">
+    <div class="w-full md:w-2/3 relative">
 
         <input type="search" placeholder="Find an idea" class="w-full rounded-xl bg-white border-none placeholder-text-gray-900 px-4 py-2 pl-8">
         <div class="absolute top-0 flex items-center h-full ml-2">
@@ -32,7 +32,7 @@
 
     <div class="ideas-container space-y-6 my-6">
         <div class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
-            <div class="border-r border-gray-100 px-5 py-8">
+            <div class="hidden md:block border-r border-gray-100 px-5 py-8">
                 <div class="text-center">
                     <div class="font-semibold text-2xl">12</div>
                     <div class="text-gray-500">Votes</div>
@@ -43,22 +43,22 @@
                     transition duration-150 ease-in font-bold text-xxs uppercase rounded-xl px-4 py-3">Vote</button>
                 </div>
             </div>
-            <div class="flex flex-1 px-2 py-6">
-            <div class="flex-none">
+            <div class="flex flex-col md:flex-row flex-1 px-2 py-6">
+            <div class="flex-none mx-2 md:mx-0">
                 <a href="#">
                     <!--    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl"> -->
                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 40 40"><path fill="#F06225" d="M20 0c11.046 0 20 8.954 20 20v14a6 6 0 0 1-6 6H21v-8.774c0-2.002.122-4.076 1.172-5.78a10 10 0 0 1 6.904-4.627l.383-.062a.8.8 0 0 0 0-1.514l-.383-.062a10 10 0 0 1-8.257-8.257l-.062-.383a.8.8 0 0 0-1.514 0l-.062.383a9.999 9.999 0 0 1-4.627 6.904C12.85 18.878 10.776 19 8.774 19H.024C.547 8.419 9.29 0 20 0Z"></path><path fill="#F06225" d="M0 21h8.774c2.002 0 4.076.122 5.78 1.172a10.02 10.02 0 0 1 3.274 3.274C18.878 27.15 19 29.224 19 31.226V40H6a6 6 0 0 1-6-6V21ZM40 2a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"></path></svg>
                 </a>
             </div>
 
-                <div class="w-full mx-4">
-                    <h4 class="text-xl font-semibold">
+                <div class="w-full flex flex-col justify-between mx-2  md:mx-4">
+                    <h4 class="text-xl mt-2 md:mt-0 font-semibold">
                      <a href="#" class="hover:underline"> A random title can go here</a>
                     </h4>
                     <div class="text-gray-600 mt-3 line-clamp-3">
                         Sed tempor massa ac felis lobortis tincidunt. Duis odio eros, sodales sed turpis dignissim, gravida feugiat leo. Maecenas mattis massa quam, vitae lobortis arcu bibendum sit amet. Mauris iaculis tempor luctus. Cras ut libero pellentesque, egestas urna ut, efficitur orci. Pellentesque vestibulum viverra dignissim. Morbi semper enim tincidunt dictum ultrices. Proin non justo ac nibh faucibus vulputate. Pellentesque lacus tellus, consequat eu pretium ut, dictum sit amet orci.
                     </div>
-                    <div class="flex items-center justify-between mt-6">
+                    <div class="flex flex-col md:flex-row md:items-center justify-between mt-6">
                         <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
                             <div>10 hours ago</div>
                             <div>&bull;</div>
@@ -68,7 +68,7 @@
                         </div>
                         <div
                             x-data="{isOpen: false }"
-                            class="flex items-center space-x-2"
+                            class="flex items-center space-x-2 mt-4 md:mt-0"
 
                         >
                             <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">
@@ -87,10 +87,21 @@
                                     @click.away="isOpen = false"
                                     @keydown.escape.window="isOpen = false"
 
-                                    class="absolute text-left w-44 font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
+                                    class="absolute text-left w-44 font-semibold bg-white shadow-dialog rounded-xl py-3 md:ml-8 top-8 md:top-6 right-0 md:left-0">
                                     <li><a href="#" class="hover:bg-gray-100 px-5 py-3 block">Mark as Spam</a></li>
                                     <li><a href="#" class="hover:bg-gray-100 px-5 py-3 block">Delete Post</a></li>
                                 </ul>
+                            </button>
+                        </div>
+                        <div class="flex items-center md:hidden mt-4 md:mt-0">
+                            <div class="bg-gray-100 text-center rounded-xl h-10 px-4 py-2 pr-8">
+                                <div class="text-sm font-bold leading-none">12</div>
+                                <div class="text-xxs font-semibold leading-none text-gray-400">Votes</div>
+                            </div>
+                            <button
+                                class="w-20 bg-gray-200 border border-gray-200 font-bold text-xxs uppercase rounded-xl hover:border-gray-400 transition duration-150
+                                ease-in px-4 py-3 -mx-4">
+                                Vote
                             </button>
                         </div>
                     </div>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -10,25 +10,25 @@
 </div>
     <div class="ideas-container space-y-6 my-6">
         <div class="idea-container bg-white rounded-xl flex mt-4">
-            <div class="flex flex-1 px-4 py-6">
-                <div class="flex-none">
+            <div class="flex flex-col md:flex-row flex-1 px-4 py-6">
+                <div class="flex-none mx-2 md:mx-4">
                     <a href="#">
                         <!--    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl"> -->
                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 40 40"><path fill="#F06225" d="M20 0c11.046 0 20 8.954 20 20v14a6 6 0 0 1-6 6H21v-8.774c0-2.002.122-4.076 1.172-5.78a10 10 0 0 1 6.904-4.627l.383-.062a.8.8 0 0 0 0-1.514l-.383-.062a10 10 0 0 1-8.257-8.257l-.062-.383a.8.8 0 0 0-1.514 0l-.062.383a9.999 9.999 0 0 1-4.627 6.904C12.85 18.878 10.776 19 8.774 19H.024C.547 8.419 9.29 0 20 0Z"></path><path fill="#F06225" d="M0 21h8.774c2.002 0 4.076.122 5.78 1.172a10.02 10.02 0 0 1 3.274 3.274C18.878 27.15 19 29.224 19 31.226V40H6a6 6 0 0 1-6-6V21ZM40 2a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"></path></svg>
                     </a>
                 </div>
 
-                <div class="w-full mx-4">
+                <div class="w-full mx-2 md:mx-4">
                     <h4 class="text-xl font-semibold">
                         <a href="#" class="hover:underline"> A random title can go here</a>
                     </h4>
                     <div class="text-gray-600 mt-3">
                         Sed tempor massa ac felis lobortis tincidunt. Duis odio eros, sodales sed turpis dignissim, gravida feugiat leo. Maecenas mattis massa quam, vitae lobortis arcu bibendum sit amet. Mauris iaculis tempor luctus. Cras ut libero pellentesque, egestas urna ut, efficitur orci. Pellentesque vestibulum viverra dignissim. Morbi semper enim tincidunt dictum ultrices. Proin non justo ac nibh faucibus vulputate. Pellentesque lacus tellus, consequat eu pretium ut, dictum sit amet orci.
                     </div>
-                    <div class="flex items-center justify-between mt-6">
+                    <div class="flex flex-col md:flex-row md:items-center justify-between mt-6">
                         <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
-                            <div class="font-bold text-gray-900">John Doe</div>
-                            <div>&bull;</div>
+                            <div class="md:block hidden  font-bold text-gray-900">John Doe</div>
+                            <div class="md:block hidden ">&bull;</div>
                             <div>10 hours ago</div>
                             <div>&bull;</div>
                             <div>Category 1</div>
@@ -37,7 +37,8 @@
                         </div>
                         <div
                             x-data="{isOpen: false }"
-                            class="flex items-center space-x-2">
+                            class="flex items-center space-x-2 mt-4 md:mt-0">
+                            <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">Open</div>
                             <button
                                 @click="isOpen = !isOpen"
                                 class="relative bg-gray-100 hover:bg-gray-200 transition duration-150 ease-in rounded-full h-7">
@@ -51,19 +52,31 @@
                                     @click.away="isOpen = false"
                                     @keydown.escape.window="isOpen = false"
 
-                                    class="absolute text-left w-44 font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
+                                    class="absolute text-left w-44 font-semibold bg-white shadow-dialog rounded-xl py-3 z-10 md:ml-8 top-8 md:top-6 right-0 md:left-0">
                                     <li><a href="#" class="hover:bg-gray-100 px-5 py-3 block">Mark as Spam</a></li>
                                     <li><a href="#" class="hover:bg-gray-100 px-5 py-3 block">Delete Post</a></li>
                                 </ul>
                             </button>
                         </div>
+                        <div class="flex items-center md:hidden mt-4 md:mt-0">
+                            <div class="bg-gray-100 text-center rounded-xl h-10 px-4 py-2 pr-8">
+                                <div class="text-sm font-bold leading-none">12</div>
+                                <div class="text-xxs font-semibold leading-none text-gray-400">Votes</div>
+                            </div>
+                            <button
+                                class="w-20 bg-gray-200 border border-gray-200 font-bold text-xxs uppercase rounded-xl hover:border-gray-400 transition duration-150
+                                ease-in px-4 py-3 -mx-4">
+                                Vote
+                            </button>
+                        </div>
+
                     </div>
                 </div>
             </div>
         </div> <!-- end idea-container -->
     </div> <!-- End ideas container -->
     <div class="buttons-container flex items-center justify-between mt-6">
-       <div class="flex items-center space-x-4 ml-6">
+       <div class="flex flex-col md:flex-row items-center space-x-4 md:ml-6">
              <div
                  x-data="{isOpen: false }"
                  class="relative">
@@ -82,16 +95,16 @@
              x-show.transition.origin.top.left="isOpen"
              @click.away="isOpen = false"
              @keydown.escape.window="isOpen = false"
-             class="absolute z-10 w-104 text-left font-semibold text-sm bg-white shadow-dialog rounded-xl mt-2">
+             class="absolute z-10 w-64 md:w-104 text-left font-semibold text-sm bg-white shadow-dialog rounded-xl mt-2">
                     <form action="#" class="space-y-4 px-4 py-6">
                         <div>
                             <textarea name="post_comment" id="post_comment" cols="30" rows="4" class="w-full text-sm bg-gray-100 rounded-xl placeholder-gray-900
                             border-none px-4 py-2" placeholder="Go ahead, don't be shy. Share your thoughts ..."></textarea>
                         </div>
-                        <div class="flex items-center space-x-3">
+                        <div class="flex flex-col md:flex-row items-center md:space-x-3">
                             <button
                                 type="button"
-                                class="flex items-center justify-center h-11 w-1/2 text-sm bg-blue
+                                class="flex items-center justify-center h-11 w-full md:w-1/2 text-sm bg-blue
                                             font-semibold  text-white rounded-xl border border-gray-200 hover:border-blue hover:bg-blue-hover transition duration-150 ease-in
                                             px-6 py-3">
 
@@ -99,8 +112,8 @@
                             </button>
                             <button
                                 type="button"
-                                class="flex items-center justify-center w-32 h-11 text-xs bg-gray-200
-                                        font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3">
+                                class="flex items-center justify-center w-full md:w-32 h-11 text-xs bg-gray-200
+                                        font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3 mt-2 md:mt-0">
                                 <svg class="text-gray-600 w-4 -rotate-45" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13" />
                                 </svg>
@@ -118,7 +131,8 @@
                 type="button"
                 @click="isOpen = !isOpen"
                 class="flex items-center justify-center w-1/2 h-11 text-sm bg-gray-200
-                                            font-semibold rounded-xl w-36 border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3">
+                                            font-semibold rounded-xl w-36 border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3 mt-2
+                                            md:mt-0">
 
                 <span >Set Status</span>
                 <svg class="w-4 h-4 ml-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
@@ -130,7 +144,7 @@
                 x-show.transition.origin.top.left="isOpen"
                 @click.away="isOpen = false"
                 @keydown.escape.window="isOpen = false"
-                class="absolute z-20 w-76 text-left font-semibold text-sm bg-white shadow-dialog rounded-xl mt-2">
+                class="absolute z-20 w-64 md:w-76 text-left font-semibold text-sm bg-white shadow-dialog rounded-xl mt-2">
                 <form action="#" class="space-y-4 px-4 py-6">
                     <div class="space-y-2">
                         <div>
@@ -200,7 +214,7 @@
         </div>
        </div>
 
-        <div class="flex items-center space-x-3 ">
+        <div class="hidden md:flex items-center space-x-3 ">
             <div class="bg-white font-semibold text-center rounded-xl px-3 py-2">
                  <div class="text-xl leading-snug">12</div>
                  <div class="text-gray-400 text-xs leading-none">Votes</div>
@@ -215,17 +229,17 @@
         </div>
     </div>
 
-        <div class="comments-container relative space-y-6 ml-24 pt-4 my-8 mt-1">
+        <div class="comments-container relative space-y-6 md:ml-22 pt-4 my-8 mt-1">
             <div class="comment-container relative bg-white rounded-xl flex mt-4">
-                <div class="flex flex-1 px-4 py-6">
-                    <div class="flex-none">
+                <div class="flex flex-col md:flex-row flex-1 px-4 py-6">
+                    <div class="flex-none mx-4">
                         <a href="#">
                             <!--    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl"> -->
                             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 40 40"><path fill="#F06225" d="M20 0c11.046 0 20 8.954 20 20v14a6 6 0 0 1-6 6H21v-8.774c0-2.002.122-4.076 1.172-5.78a10 10 0 0 1 6.904-4.627l.383-.062a.8.8 0 0 0 0-1.514l-.383-.062a10 10 0 0 1-8.257-8.257l-.062-.383a.8.8 0 0 0-1.514 0l-.062.383a9.999 9.999 0 0 1-4.627 6.904C12.85 18.878 10.776 19 8.774 19H.024C.547 8.419 9.29 0 20 0Z"></path><path fill="#F06225" d="M0 21h8.774c2.002 0 4.076.122 5.78 1.172a10.02 10.02 0 0 1 3.274 3.274C18.878 27.15 19 29.224 19 31.226V40H6a6 6 0 0 1-6-6V21ZM40 2a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"></path></svg>
                         </a>
                     </div>
 
-                    <div class="w-full mx-4">
+                    <div class="w-full md:mx-4">
                     <!--     <h4 class="text-xl font-semibold">
                             <a href="#" class="hover:underline"> A random title can go here</a>
                         </h4> -->
@@ -254,7 +268,8 @@
                                         @click.away="isOpen = false"
                                         @keydown.escape.window="isOpen = false"
 
-                                        class="absolute text-left w-44 font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
+                                        class="absolute text-left w-44 font-semibold bg-white shadow-dialog rounded-xl z-10 py-3 md:ml-8
+                                        top-8 md:top-6 right-0 md:left-0">
                                         <li><a href="#" class="hover:bg-gray-100 px-5 py-3 block">Mark as Spam</a></li>
                                         <li><a href="#" class="hover:bg-gray-100 px-5 py-3 block">Delete Post</a></li>
                                     </ul>
@@ -266,8 +281,8 @@
 
             </div> <!-- end comment-container -->
             <div class="is-admin comment-container relative bg-white rounded-xl flex mt-4">
-                <div class="flex flex-1 px-4 py-6">
-                    <div class="flex-none">
+                <div class="flex flex-col md:flex-row flex-1 px-4 py-6">
+                    <div class="flex-none mx-4">
                         <a href="#">
                             <!--    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl"> -->
                             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 40 40"><path fill="#F06225" d="M20 0c11.046 0 20 8.954 20 20v14a6 6 0 0 1-6 6H21v-8.774c0-2.002.122-4.076 1.172-5.78a10 10 0 0 1 6.904-4.627l.383-.062a.8.8 0 0 0 0-1.514l-.383-.062a10 10 0 0 1-8.257-8.257l-.062-.383a.8.8 0 0 0-1.514 0l-.062.383a9.999 9.999 0 0 1-4.627 6.904C12.85 18.878 10.776 19 8.774 19H.024C.547 8.419 9.29 0 20 0Z"></path><path fill="#F06225" d="M0 21h8.774c2.002 0 4.076.122 5.78 1.172a10.02 10.02 0 0 1 3.274 3.274C18.878 27.15 19 29.224 19 31.226V40H6a6 6 0 0 1-6-6V21ZM40 2a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"></path></svg>
@@ -282,7 +297,7 @@
                         <div class="text-gray-600 mt-3">
                             Sed tempor massa ac felis lobortis tincidunt. Duis odio eros, sodales sed turpis dignissim, gravida feugiat leo. Maecenas mattis massa quam, vitae lobortis arcu bibendum sit amet. Mauris iaculis tempor luctus. Cras ut libero pellentesque, egestas urna ut, efficitur orci. Pellentesque vestibulum viverra dignissim. Morbi semper enim tincidunt dictum ultrices. Proin non justo ac nibh faucibus vulputate. Pellentesque lacus tellus, consequat eu pretium ut, dictum sit amet orci.
                         </div>
-                        <div class="flex items-center justify-between mt-6">
+                        <div class="flex flex-col md:flex-row md:items-center justify-between mt-6">
                             <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
                                 <div class="font-bold text-blue">Andrea</div>
                                 <div>&bull;</div>
@@ -315,16 +330,13 @@
                     </div>
 
                     <div class="w-full mx-4">
-                        <!--     <h4 class="text-xl font-semibold">
-                                <a href="#" class="hover:underline"> A random title can go here</a>
-                            </h4> -->
                         <div class="text-gray-600 mt-3">
                             Sed tempor massa ac felis lobortis tincidunt. Duis odio eros, sodales sed turpis dignissim, gravida feugiat leo. Maecenas mattis massa quam, vitae lobortis arcu bibendum sit amet. Mauris iaculis tempor luctus. Cras ut libero pellentesque, egestas urna ut, efficitur orci. Pellentesque vestibulum viverra dignissim. Morbi semper enim tincidunt dictum ultrices. Proin non justo ac nibh faucibus vulputate. Pellentesque lacus tellus, consequat eu pretium ut, dictum sit amet orci.
                         </div>
                         <div class="flex items-center justify-between mt-6">
                             <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
-                                <div class="font-bold text-gray-900">John Doe</div>
-                                <div>&bull;</div>
+                                <div class="hidden md:block font-bold text-gray-900">John Doe</div>
+                                <div class="hidden md:block">&bull;</div>
                                 <div>10 hours ago</div>
                             </div>
                             <div
