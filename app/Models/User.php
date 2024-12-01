4b41cd105504dd766780a09d0e085bac473a4f6f
poljak07 <acmeta4@gmail.com>
2024-12-01 16:57:32 +0100

    Gravatar Images

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,14 @@ class User extends Authenticatable
     {
         return $this->hasMany(Idea::class);
     }
+
+    public function getAvatar()
+    {
+
+        return 'https://www.gravatar.com/avatar/'
+              .md5($this->email)
+                .'?s=200'
+                .'&d=mp';
+    }
+
 }
