23fbb7e933d694bd944fc09895aa208bb12f0391
poljak07 <acmeta4@gmail.com>
2024-12-02 18:44:07 +0100

    Add Statuses

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -32,4 +32,11 @@ class Idea extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+
+    }
+    
 }
