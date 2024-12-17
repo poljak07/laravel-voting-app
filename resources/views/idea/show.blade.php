bc8124ac408ff6539f03c33b0c1419f50f8c9a1e
poljak07 <acmeta4@gmail.com>
2024-12-17 14:48:48 +0100

    Refactor Modals to Blade Component

--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -15,23 +15,7 @@
         :votesCount="$votesCount"
     />
 
-    @can('update', $idea)
-        <livewire:edit-idea
-        :idea="$idea"
-        />
-    @endcan
-
-    @can('delete', $idea)
-    <livewire:delete-idea :idea="$idea"/>
-    @endcan
-
-    @auth
-    <livewire:mark-idea-as-spam :idea="$idea" />
-    @endauth
-
-    @admin
-    <livewire:mark-idea-as-not-spam :idea="$idea" />
-    @endadmin
+    <x-modals-container :idea="$idea" />
 
 
 
