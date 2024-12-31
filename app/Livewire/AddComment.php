88e7067ed0fc483e902d9d3d7c8814a2d1be70c2
poljak07 <acmeta4@gmail.com>
2024-12-31 15:43:02 +0100

    View Notifications

--- app/Livewire/AddComment.php
+++ app/Livewire/AddComment.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Comment;
 use App\Models\Idea;
+use App\Notifications\CommentAdded;
 use Livewire\Component;
 
 class AddComment extends Component
@@ -27,7 +28,7 @@ class AddComment extends Component
 
         $this->validate();
 
-        Comment::create([
+        $newComment = Comment::create([
             'user_id' => auth()->id(),
             'idea_id' => $this->idea->id,
             'status_id' => 1,
@@ -35,6 +36,8 @@ class AddComment extends Component
         ]);
 
         $this->reset('comment');
+
+        $this->idea->user->notify(new CommentAdded($newComment));
 
         $this->dispatch('commentWasAdded', 'Comment was posted!');
 
---
+++ resources/views/livewire/comment-notifications.blade.php
@@ -0,0 +1,49 @@
+<div x-data="{isOpen: false}" class="relative">
+    <button @click="isOpen = !isOpen
+        if(isOpen) {
+            Livewire.dispatch('getNotifications')
+        }
+       "
+    >
+        <svg class="h-8 w-8 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+        </svg>
+        <div class="absolute rounded-full bg-red text-white text-xxs w-6 h-6 flex justify-center items-center border-2 -top-1 -right-1"
+        >8</div>
+    </button>
+    <ul
+        x-show.transition.origin.top="isOpen"
+        @click.away="isOpen = false"
+        x-cloak
+        @keydown.escape.window="isOpen = false"
+        class="absolute text-left text-gray-700 text-sm w-76 md:w-96 bg-white shadow-dialog rounded-xl max-h-128 overflow-y-auto z-10 -right-28
+                                    md:-right-12"
+    >
+        @foreach ($notifications as $notification)
+            <li>
+                <a
+                    href="{{ route('idea.show', $notification->data['idea_slug']) }}"
+                    {{-- @click.prevent="
+                        isOpen = false
+                    " --}}
+                    class="flex hover:bg-gray-100 transition duration-150 ease-in px-5 py-3"
+                >
+                    <img src="{{ $notification->data['user_avatar'] }}" class="rounded-xl w-10 h-10" alt="avatar">
+                    <div class="ml-4">
+                        <div class="line-clamp-6">
+                            <span class="font-semibold">{{ $notification->data['user_name'] }}</span> commented on
+                            <span class="font-semibold">{{ $notification->data['idea_title'] }}</span>:
+                            <span>"{{ $notification->data['comment_body'] }}"</span>
+                        </div>
+                        <div class="text-xs text-gray-500 mt-2">{{ $notification->created_at->diffForHumans() }}</div>
+                    </div>
+                </a>
+            </li>
+        @endforeach
+        <li class="border-t border-gray-300 text-center">
+            <button
+                href="#"
+                class="w-full block font-semibold hover:bg-gray-100 px-5 py-4">Mark all as read</button>
+        </li>
+    </ul>
+</div>
